6668aa51b73a157f30d43694e530cd6fdef13628
대식 <sejjang2eric@naver.com>
2023-04-20 10:57:48 +0900

    올리기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [box, setBox] = useState([
+    { id: 1, text: "react를 배워봅시다." },
+  ])
+
+  const [text, setText] = useState("")
+
+  const textAdd = (e) => {
+    setText(e.target.value)
+  }
+
+  const addButton = () => {
+    const addBox = {
+      id : box.length + 1,
+      text,
+    }
+
+    setBox([...box, addBox])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='top'>
+        <input
+          value={text}
+          onChange={(e) => { textAdd(e) }}></input>
+        <button onClick={(addButton)}>추가하기</button>
+      </div>
+      <h2 className='title'>Todo List</h2>
+      <div className='list'>
+        {
+          box.map((a) => {
+            return (
+              <div key={a.id} className='box'>{a.text}</div>
+            )
+          })
+        }
+      </div>
+    </>
   );
 }
 
